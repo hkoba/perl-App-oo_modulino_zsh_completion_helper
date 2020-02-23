f5d75041e77ce7ebefd0d7af23447ba15b41087f
Kobayasi, Hiroaki <buribullet@gmail.com>
2020-02-23 18:52:08 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::oo\_modulino\_zsh\_completion\_helper - It's new $module
+
+# SYNOPSIS
+
+    use App::oo_modulino_zsh_completion_helper;
+
+# DESCRIPTION
+
+App::oo\_modulino\_zsh\_completion\_helper is ...
+
+# LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kobayasi, Hiroaki <buribullet@gmail.com>
---
+++ lib/App/oo_modulino_zsh_completion_helper.pm
@@ -0,0 +1,39 @@
+package App::oo_modulino_zsh_completion_helper;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::oo_modulino_zsh_completion_helper - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::oo_modulino_zsh_completion_helper;
+
+=head1 DESCRIPTION
+
+App::oo_modulino_zsh_completion_helper is ...
+
+=head1 LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kobayasi, Hiroaki E<lt>buribullet@gmail.comE<gt>
+
+=cut
+
